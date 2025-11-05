506fce89c77c18dd483e72bcade630055ea80ae9
Deepak Sharma <dsharma2828@gmail.com>
2025-11-05 23:21:21 +0530

    Config server and Eureka server created

---
+++ config-server/src/test/java/com/inventrack/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventrack.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
