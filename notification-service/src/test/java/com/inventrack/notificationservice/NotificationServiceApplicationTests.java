d4bc2446558571c25a447b724c3634533e640b35
Deepak Sharma <dsharma2828@gmail.com>
2025-11-11 23:31:47 +0530

    Notification Service in process

---
+++ notification-service/src/test/java/com/inventrack/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventrack.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
